b07c52f82eecdd255b0264790735b8aaec5b8587
Pickle <anthonymarrero314@gmail.com>
2025-08-08 11:47:27 -0400

    Initial commit. Hooray.

---
+++ src/main/java/net/picklemod/Pickled.java
@@ -0,0 +1,17 @@
+package net.picklemod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Pickled implements ModInitializer {
+	public static final String MOD_ID = "pickled";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/picklemod/PickledModClient.java
@@ -0,0 +1,12 @@
+package net.picklemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PickledModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient(){
+
+    }
+
+}
